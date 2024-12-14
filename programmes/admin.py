6661603212dd3220b3fc6a8ffab98adb1b84ad00
Udev <udev352@gmail.com>
2024-12-14 20:20:51 +0000

    commit003

--- programmes/admin.py
+++ programmes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Niveau, Matiere, Salle, Batiment, Utilisateur, Notification,EmploiDuTemps, ReleveDeNote, Archive, EspaceEchange, Message, CahierAbsence
+
+admin.site.register(Niveau)
+admin.site.register(Matiere)
+admin.site.register(Salle)
+admin.site.register(Batiment)
+admin.site.register(Utilisateur)
+admin.site.register(Notification)
+admin.site.register(EmploiDuTemps)
+admin.site.register(ReleveDeNote)
+admin.site.register(Archive)
+admin.site.register(EspaceEchange)
+admin.site.register(Message)
+admin.site.register(CahierAbsence)
